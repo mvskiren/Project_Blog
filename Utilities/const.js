72ff3be2bdb3f530cf1fe61d2dd8145e472f48b9
Sai Kiran Marisetti <kiranmvs07@gmail.com>
2022-11-06 23:16:01 +0530

    refactor: refactoring code by creating resuable components

--- Utilities/const.js
+++ Utilities/const.js
@@ -2,49 +2,67 @@ export let sampleResponse = [
   {
     title: 'Optimizing SSR Pages',
     authorName: 'Kiran',
-    description: 'Lorem Ipsum here...',
+    description: `Lorem ipsum dolor sit amet consecoptimizationur adipisicing elit.
+Ducimus aperiam itaque assumenda. Natus omnis tenetur, repellendus,
+at officiis accusantium eos, et cupiditate quod labore rem expedita
+consequatur beatae id doloremque!`,
     imageUrl: '',
     tags: ['Optimization', 'Design'],
-    readTime: '5mins',
+    readTime: '',
   },
   {
     title: 'Optimizing SSR Pages2',
     authorName: 'Kiran',
-    description: 'Lorem Ipsum here...',
+    description: `Lorem ipsum dolor sit amet consecoptimizationur adipisicing elit.
+Ducimus aperiam itaque assumenda. Natus omnis tenetur, repellendus,
+at officiis accusantium eos, et cupiditate quod labore rem expedita
+consequatur beatae id doloremque!`,
     imageUrl: '',
     tags: ['Optimization', 'Design'],
-    readTime: '5mins',
+    readTime: '',
   },
   {
     title: 'Optimizing SSR Pages3',
     authorName: 'Kiran',
-    description: 'Lorem Ipsum here...',
+    description: `Lorem ipsum dolor sit amet consecoptimizationur adipisicing elit.
+Ducimus aperiam itaque assumenda. Natus omnis tenetur, repellendus,
+at officiis accusantium eos, et cupiditate quod labore rem expedita
+consequatur beatae id doloremque!`,
     imageUrl: '',
     tags: ['Optimization', 'Design'],
-    readTime: '5mins',
+    readTime: '',
   },
   {
     title: 'Optimizing SSR Pages4',
     authorName: 'Kiran',
-    description: 'Lorem Ipsum here...',
+    description: `Lorem ipsum dolor sit amet consecoptimizationur adipisicing elit.
+Ducimus aperiam itaque assumenda. Natus omnis tenetur, repellendus,
+at officiis accusantium eos, et cupiditate quod labore rem expedita
+consequatur beatae id doloremque!`,
     imageUrl: '',
     tags: ['Optimization', 'Design'],
-    readTime: '5mins',
+    readTime: '',
   },
   {
     title: 'Optimizing SSR Pages5',
     authorName: 'Kiran',
-    description: 'Lorem Ipsum here...',
+    description: `Lorem ipsum dolor sit amet consecoptimizationur adipisicing elit.
+Ducimus aperiam itaque assumenda. Natus omnis tenetur, repellendus,
+at officiis accusantium eos, et cupiditate quod labore rem expedita
+consequatur beatae id doloremque!`,
     imageUrl: '',
     tags: ['Optimization', 'Design'],
-    readTime: '5mins',
+    readTime: '',
   },
   {
     title: 'Optimizing SSR Pages6',
     authorName: 'Kiran',
-    description: 'Lorem Ipsum here...',
+    description: `Lorem ipsum dolor sit amet consecoptimizationur adipisicing elit.
+Ducimus aperiam itaque assumenda. Natus omnis tenetur, repellendus,
+at officiis accusantium eos, et cupiditate quod labore rem expedita
+consequatur beatae id doloremque!`,
     imageUrl: '',
     tags: ['Optimization', 'Design'],
-    readTime: '5mins',
+    readTime: '',
   },
 ];
--- components/PostGrid/PostGrid.js
+++ components/PostGrid/PostGrid.js
@@ -1,44 +1,29 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Classes from './PostGrid.module.scss';
-import Filter from '../Filter/Filter';
+import DropDown from '../Filters/DropDown/DropDown';
 import Modal from '../ResuableComponents/Modal/Modal';
 import SearchIcon from '../../Utilities/Icons/SearchIcon';
 import BlogCard from '../ResuableComponents/BlogCard/BlogCard';
+import Search from '../Filters/Search/Search';
 import { sampleResponse } from '../../Utilities/const';
 
 function PostGrid() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const toggleModal = () => {
+    setIsOpen(true);
+  };
+
   return (
     <React.Fragment>
       <div className={Classes.postGrid}>
         <div className={Classes.postGrid__filters}>
-          <div
-            style={{
-              marginRight: 'auto',
-              fontSize: '1.5rem',
-              color: 'gray',
-              fontWeight: 500,
-            }}
-          >
+          <div className={Classes.postGrid__filters__sectionTitle}>
             BlogPosts
           </div>
-          <div className={Classes.postGrid__filters__search}>
-            <input
-              className={Classes.postGrid__search}
-              type="text"
-              name="search"
-              id="modal_search"
-              placeholder="Search posts,tags,authors..."
-              readOnly
-              onFocus={() => setIsOpen(true)}
-            />
-            <div className={Classes.postGrid__filters__search__icon}>
-              <SearchIcon />
-            </div>
-          </div>
+          <Search setIsOpen={toggleModal} />
           <div className={Classes.postGrid__filters__category}>
-            <Filter />{' '}
+            <DropDown />{' '}
           </div>
         </div>
         <div className={Classes.postGrid__posts}>
